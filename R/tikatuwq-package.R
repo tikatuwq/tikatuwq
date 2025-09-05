a8b14aa135160e429faa7190006cd22a16d7bcce
Vinicius <vinisaraiva@gmail.com>
2025-09-05 18:14:46 -0300

    docs: usar _PACKAGE; imports de nivel de pacote; globalVariables()

---
+++ R/tikatuwq-package.R
@@ -0,0 +1,20 @@
+#' tikatuwq: Ferramentas para analise de qualidade da agua (Brasil)
+#'
+#' Indices (IQA CETESB/NSF, IET Carlson/Lamparelli), checagem CONAMA 357/2005,
+#' visualizacoes e texto analitico automatico (rule-based).
+#'
+#' @keywords internal
+#'
+#' @importFrom stats approx median sd
+#' @importFrom utils data
+#' @importFrom dplyr coalesce n all_of
+#' @importFrom readr read_lines read_delim parse_number cols col_character locale
+#' @importFrom stringr str_count str_replace_all
+#' @importFrom lubridate ymd dmy
+"_PACKAGE"
+
+# Silencia NOTEs de variaveis NSE usadas com dplyr/ggplot2
+utils::globalVariables(c(
+  ".data","IQA","IQA_med","param_ok","ok","parametro","n_viol","total",
+  "ponto","data","valor"
+))
